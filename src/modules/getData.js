fcb5974d5bd4099f65bba032ca7d7ca6853f8baa
qerxx <100464629+qerxx@users.noreply.github.com>
2022-05-20 21:14:47 +0300

    upload_lesson_3

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,8 +1,10 @@
-const getData = () => {
-    return fetch('https://test-ozon-f9a8a-default-rtdb.firebaseio.com/goods.json')
-    .then((response) => {
-        return response.json()
-    })
+const getData = (str) => {
+    return fetch(
+        `https://test-ozon-f9a8a-default-rtdb.firebaseio.com/goods.json?${str ? `search=${str}` : ''}`
+        )
+        .then((response) => {
+            return response.json()
+        })
 }
 
 export default getData
